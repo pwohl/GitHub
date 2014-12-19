1da502ac8a0f6688e43f3bbe2b59f7cf52d72243
pwohl <pwohland@gmail.com>
2014-12-19 09:18:55 +0000

    Update with NavBar

--- BMIcalculator/ui.R
+++ BMIcalculator/ui.R
@@ -1,4 +1,6 @@
-shinyUI(pageWithSidebar(
+shinyUI(navbarPage("Navbar!",
+                   tabPanel("BMI",
+        pageWithSidebar(
         headerPanel("Body-Mass-Index (BMI) calculator"),
         sidebarPanel(
                 h3('Input'),
@@ -10,6 +12,7 @@ shinyUI(pageWithSidebar(
                 p('Maximum input weight: 640 kg (heavier than Jon Brower Minnoch)'),
                 p('Maximum input height: 275 cm (taller than Robert Pershing Wadlow)'),
                 submitButton('GO!')
+        
         ),
         mainPanel(
                 h3('Output'),
@@ -20,7 +23,13 @@ shinyUI(pageWithSidebar(
                 h3('Your BMI is:'),
                 verbatimTextOutput("BMIval"),
                 p('A BMI between 18.5 and 25 is usually considered healthy, a BMI lower than 18.5 suggests you are underweight, 
-                 a BMI above 25 suggests you might be overweight.')
-               
-        )
-))
+                 a BMI above 25 suggests you might be overweight.')))),
+                
+                tabPanel("Documentation",
+                         h3('Documentation'),
+                         p('This shiny-app lets you calculate your Body-Mass-Index (BMI) to evaluate whether you have a healthy weight.
+                        To calculate your BMI you only need to know your weight in kg and your height in cm, input them in the input side in the fields provided 
+                        and the app will do the calculation for you.
+                        A BMI between 18.5 and 25 is usually considered healthy.'))))
+                         
+     
